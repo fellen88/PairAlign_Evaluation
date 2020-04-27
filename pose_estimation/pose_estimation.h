23850f70e27ee79125663a2bae682c46bdd873f5
fellenB <fellenB@outlook.com>
2020-04-27 15:33:39 +0800

    delete segmentation

--- pose_estimation/pose_estimation.h
+++ pose_estimation/pose_estimation.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "registration.h"
+#include "segmentation.h"
 
 #ifdef __DLLEXPORT
 #define __DLL_INTERFACE _declspec(dllexport)    // 导出函数 - 生成dll文件时使用
@@ -11,6 +12,7 @@ class  __DLL_INTERFACE PoseEstimation
 {
 	public:
 	Registration *p_registration_;
+	Segmentation *p_segmentation_;
 	Eigen::Matrix4f final_transform;
 	Eigen::Matrix4f sac_transform;
 	Eigen::Matrix4f icp_transform;
@@ -21,7 +23,6 @@ class  __DLL_INTERFACE PoseEstimation
 	~PoseEstimation();
 
 	void Init();
-	void Compute();
 	Eigen::Matrix4f Compute(PointCloud::Ptr source, PointCloud::Ptr target);
 };
 

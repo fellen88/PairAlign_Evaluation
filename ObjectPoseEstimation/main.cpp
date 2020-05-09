ddb9ae4e449aed24acac03f56435aa86330952ca
fellenB <fellenB@outlook.com>
2020-05-09 15:21:36 +0800

    1. dll test

--- ObjectPoseEstimation/main.cpp
+++ ObjectPoseEstimation/main.cpp
@@ -1,6 +1,17 @@
 ﻿#include <stdlib.h>
+#include "..\pose_estimation\pose_estimation.h"
+#pragma comment (lib, "..\\X3PYPlugin\\bin\\x64\\release\\pose_estimation.lib")
+
 int main(int argc, char **argv)
 {
+	PoseEstimation *pose_estimation_ =GetInstance();
+
+	int n_count = 0;
+	while (n_count < 2)
+	{
+		pose_estimation_->Compute();
+		n_count++;
+	}
 	system("pause");
 	return 0;
 }
